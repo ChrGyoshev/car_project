2aabb206babe23fb7955deea56111c3e481e824e
ChrGyoshev <krischan@abv.bg>
2024-08-31 11:11:02 +0300

    refactoring some frontend files, added bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
-    <h1>Hello</h1>
+      <h1 className="text-danger">Hello</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
