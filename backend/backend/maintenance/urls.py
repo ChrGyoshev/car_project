721af87ebee3324de60b293c38beb4834dabc2fe
ChrGyoshev <krischan@abv.bg>
2024-09-07 18:02:10 +0300

    added list all maintenance for specific car view

--- backend/backend/maintenance/urls.py
+++ backend/backend/maintenance/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from backend.maintenance.views import CreateMaintenanceView
+from backend.maintenance.views import CreateMaintenanceView, ListCarMaintenancesView
 
 
 urlpatterns = [
-    path('create/<int:pk>', CreateMaintenanceView.as_view())
+    path('create/<int:pk>', CreateMaintenanceView.as_view()),
+    path('list/<int:pk>', ListCarMaintenancesView.as_view()),
 ]
--- backend/backend/maintenance/views.py
+++ backend/backend/maintenance/views.py
@@ -6,12 +6,12 @@ from rest_framework.exceptions import PermissionDenied
 
 from backend.maintenance.serializers import MaintenanceSerializer
 from backend.cars.models import Car
-from mixins.mixin import CheckCarOwnerMixin, GetUserTokenMixin
+from mixins.mixin import CheckCarOwnerMixin, GetUserTokenMixin, PermissionMixin
 
 
 User= get_user_model()
 
-class CreateMaintenanceView(CheckCarOwnerMixin,GetUserTokenMixin,APIView):
+class CreateMaintenanceView(PermissionMixin,CheckCarOwnerMixin,GetUserTokenMixin,APIView):
     def post(self,request,*args,**kwargs):
         user_from_token = self.get_user_from_token(request)
         car_owned = self.check_owner(user_from_token)
@@ -22,3 +22,13 @@ class CreateMaintenanceView(CheckCarOwnerMixin,GetUserTokenMixin,APIView):
             return Response(serializer.data)
         raise PermissionDenied("You do not have ownership of the car")
 
+
+class ListCarMaintenancesView(PermissionMixin,CheckCarOwnerMixin,GetUserTokenMixin,APIView):
+    def get(self,request,*args,**kwargs):
+        user_from_token = self.get_user_from_token(request)
+        car_owned = self.check_owner(user_from_token)
+        if car_owned:
+            maintenance = car_owned.maintenance.all()
+            serializer = MaintenanceSerializer(maintenance, many=True)
+            return Response(serializer.data)
+        raise PermissionDenied("You're not a owner of the car!")
